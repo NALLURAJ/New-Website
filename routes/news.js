b2a40582fcb2b33ada52baa0a850f6e983e9e480
Nalluraj <nallurajbabu1924@example.com>
2024-01-07 22:11:19 +0530

    first commit

---
+++ routes/news.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const axios = require('axios')
+const newsr=express.Router()
+const moment = require('moment')
+const math = require('math')
+
+
+newsr.get('/',async(req,res)=>{
+    try {
+        var url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=bafa722891d44e94a60c751a0111e596';
+
+        const news_get =await axios.get(url)
+        res.render('news',{articles:news_get.data.articles})
+
+
+
+
+    } catch (error) {
+        if(error.response){
+            console.log(error)
+        }
+
+    }
+})
+
+newsr.post('/search',async(req,res)=>{
+    const search=req.body.search
+    // console.log(req.body.search)
+
+    try {
+        var url = `http://newsapi.org/v2/everything?q=${search}&apiKey=bafa722891d44e94a60c751a0111e596`
+
+        const news_get =await axios.get(url)
+        res.render('news',{articles:news_get.data.articles})
+
+
+
+
+
+    } catch (error) {
+        if(error.response){
+            console.log(error)
+        }
+
+    }
+})
+
+
+module.exports=newsr
